ddc79b030653c9514e9022bac6160e86b34bb4a6
Serg03 <55018504+Serg03@users.noreply.github.com>
2020-04-23 18:51:53 +0300

    Add files via upload

---
+++ ТРИ_Вектора_Практическая_3/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ТРИ_Вектора_Практическая_3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			double ax, ay, az, bx, by, bz;
+			ax = Convert.ToDouble(textBox1.Text);
+			ay = Convert.ToDouble(textBox2.Text);
+			az = Convert.ToDouble(textBox3.Text);
+			bx = Convert.ToDouble(textBox4.Text);
+			by = Convert.ToDouble(textBox5.Text);
+			bz = Convert.ToDouble(textBox6.Text);
+			Vector vec = new Vector(ax, ay, az, bx, by, bz);
+			textBox7.Text = vec.CX(ax, bx).ToString("F3");
+			textBox8.Text = vec.CY(ay, by).ToString("F3");
+			textBox9.Text = vec.CZ(az, bz).ToString("F3");
+			textBox10.Text = vec.AB(ax, ay, az, bx, by, bz).ToString("F3");
+			textBox11.Text = vec.BC(ax, ay, az, bx, by, bz).ToString("F3");
+			textBox12.Text = vec.AC(ax, ay, az, bx, by, bz).ToString("F3");
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = null;
+			textBox2.Text = null;
+			textBox3.Text = null;
+			textBox4.Text = null;
+			textBox5.Text = null;
+			textBox6.Text = null;
+			textBox7.Text = null;
+			textBox8.Text = null;
+			textBox9.Text = null;
+			textBox10.Text = null;
+			textBox11.Text = null;
+			textBox12.Text = null;
+		}
+	}
+	class Vector 
+	{
+		
+		public Vector()
+		{
+		}
+		public Vector(double ax, double ay, double az, double bx, double by, double bz)
+		{		
+		}
+		  public double CX(double ax, double bx)
+		{
+			return  (ax + bx) * ax;
+		}
+		  public double CY(double ay, double by)
+		{
+			return  (ay + by) * ay;
+		}
+		  public double CZ(double az, double bz)
+		{
+			return (az + bz) * az;
+		}
+
+		public double AB(double ax, double ay, double az, double bx, double by, double bz)
+{
+return 57.3*Math.Acos((ax*bx + ay*by + az*bz)/(Math.Sqrt(ax*ax+ay*ay+az*az) * Math.Sqrt(bx * bx + by * by + bz * bz)));
+}
+        public double BC(double ax, double ay, double az, double bx, double by, double bz)
+{
+return 57.3*Math.Acos((CX(ax, bx) * bx + CY(ay, by) * by + CZ(az, bz) * bz) / (Math.Sqrt(CX(ax, bx) * CX(ax, bx) + CY(ay, by) * CY(ay, by) + CZ(az, bz) * CZ(az, bz)) * Math.Sqrt(bx * bx + by * by + bz * bz)));
+}
+        public double AC(double ax, double ay, double az, double bx, double by, double bz)
+{
+return 57.3*Math.Acos((ax * bx + ay * by + az * bz) / (Math.Sqrt(ax * ax + ay * ay + az * az) * Math.Sqrt(bx * bx + by * by + bz * bz)));
+}
+	}
+	class Ugol : Vector 
+	{
+	
+	}
+}
---
+++ ТРИ_Вектора_Практическая_3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("ТРИ_Вектора_Практическая_3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ТРИ_Вектора_Практическая_3")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6655e091-323b-4f1e-91ab-2d19a5839f27")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
