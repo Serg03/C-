6fcbb4e5649760d7e4acb1846528f70fceee9e81
Serg03 <55018504+Serg03@users.noreply.github.com>
2020-06-07 13:10:57 +0300

    6_Лабораторная_С#

---
+++ Потоки .NET/Потоки .NET/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Потоки.NET
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(407, 413);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(94, 40);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Начать";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(12, 21);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(502, 21);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "* Вычислить 10-ю степень двойки сложением, умножением";
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Cross;
+			this.dataGridView1.Location = new System.Drawing.Point(595, 12);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.dataGridView1.Size = new System.Drawing.Size(430, 525);
+			this.dataGridView1.TabIndex = 2;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(32, 280);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(44, 20);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(174, 280);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(44, 20);
+			this.textBox2.TabIndex = 4;
+			this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(316, 280);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(44, 20);
+			this.textBox3.TabIndex = 5;
+			this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(12, 101);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(429, 21);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "* Реализовать возможность задавать приоритет";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(112, 203);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(194, 21);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Выберите приоритет";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(13, 246);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(94, 21);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Сложения";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.Location = new System.Drawing.Point(142, 246);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(106, 21);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Умножения";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(254, 246);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(184, 21);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "Возведения в степень";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label7.Location = new System.Drawing.Point(12, 53);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(307, 21);
+			this.label7.TabIndex = 11;
+			this.label7.Text = "и простосто возведением в степень.";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label8.Location = new System.Drawing.Point(12, 134);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(294, 21);
+			this.label8.TabIndex = 12;
+			this.label8.Text = "каждого из порожденных потоков";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label9.Location = new System.Drawing.Point(13, 324);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(93, 22);
+			this.label9.TabIndex = 13;
+			this.label9.Text = "1) Highest";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label10.Location = new System.Drawing.Point(13, 370);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(143, 22);
+			this.label10.TabIndex = 14;
+			this.label10.Text = "2) AboveNormal";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label11.Location = new System.Drawing.Point(13, 415);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(93, 22);
+			this.label11.TabIndex = 15;
+			this.label11.Text = "3) Normal";
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label12.Location = new System.Drawing.Point(13, 459);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(144, 22);
+			this.label12.TabIndex = 16;
+			this.label12.Text = "4) BelowNormal";
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label13.Location = new System.Drawing.Point(13, 500);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(90, 22);
+			this.label13.TabIndex = 17;
+			this.label13.Text = "5) Lowest";
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label14.Location = new System.Drawing.Point(312, 205);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(228, 19);
+			this.label14.TabIndex = 18;
+			this.label14.Text = "(Пустое поле означает Normal)";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+			this.ClientSize = new System.Drawing.Size(1087, 549);
+			this.Controls.Add(this.label14);
+			this.Controls.Add(this.label13);
+			this.Controls.Add(this.label12);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label14;
+	}
+}
+
---
+++ Потоки .NET/Потоки .NET/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Потоки.NET
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.CenterToScreen();
+
+			var column1 = new DataGridViewColumn();
+			column1.HeaderText = "Сложение"; //текст в шапке
+			column1.Width = 100; //ширина колонки
+			//column1.ReadOnly = true; //значение в этой колонке нельзя править
+			column1.Name = "sloj"; //текстовое имя колонки, его можно использовать вместо обращений по индексу
+			column1.Frozen = true; //флаг, что данная колонка всегда отображается на своем месте
+			column1.CellTemplate = new DataGridViewTextBoxCell(); //тип нашей колонки
+
+			var column2 = new DataGridViewColumn();
+			column2.HeaderText = "Умножение"; //текст в шапке
+			column2.Width = 100; //ширина колонки
+			//column2.ReadOnly = true; //значение в этой колонке нельзя править
+			column2.Name = "umnoj"; //текстовое имя колонки, его можно использовать вместо обращений по индексу
+			column2.Frozen = true; //флаг, что данная колонка всегда отображается на своем месте
+			column2.CellTemplate = new DataGridViewTextBoxCell(); //тип нашей колонки
+
+			var column3 = new DataGridViewColumn();
+			column3.HeaderText = "Возведение в степень"; //текст в шапке
+			column3.Width = 200; //ширина колонки
+			//column3.ReadOnly = true; //значение в этой колонке нельзя править
+			column3.Name = "stepen"; //текстовое имя колонки, его можно использовать вместо обращений по индексу
+			column3.Frozen = true; //флаг, что данная колонка всегда отображается на своем месте
+			column3.CellTemplate = new DataGridViewTextBoxCell(); //тип нашей колонки
+
+			dataGridView1.Columns.Add(column1);
+			dataGridView1.Columns.Add(column2);
+			dataGridView1.Columns.Add(column3);
+
+		}
+
+
+
+		private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char number = e.KeyChar;
+			if ((e.KeyChar <= 48 || e.KeyChar >= 54) && (e.KeyChar <= 07 || e.KeyChar >= 09))
+			{
+				e.Handled = true;
+			}
+			textBox1.MaxLength = 1;
+		}
+
+		private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char number = e.KeyChar;
+			if ((e.KeyChar <= 48 || e.KeyChar >= 54) && (e.KeyChar <= 07 || e.KeyChar >= 09))
+			{
+				e.Handled = true;
+			}
+			textBox1.MaxLength = 1;
+		}
+
+		private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char number = e.KeyChar;
+			if ((e.KeyChar <= 48 || e.KeyChar >= 54) && (e.KeyChar <= 07 || e.KeyChar >= 09))
+			{
+				e.Handled = true;
+			}
+			textBox1.MaxLength = 1;
+		}
+
+		static object loker = new object();
+		public void mythread1(){
+
+			
+			for (int i = 1; i <= 512; i++)
+				{lock (loker)
+			{
+					dataGridView1.Rows.Add(i * 2, "", "");
+				}
+			}
+		}
+		public void mythread2(){
+			
+				int a = 1;
+				for (int i = 0; i < 10; i++)
+				{lock (loker)
+			{
+					a = a * 2;
+					dataGridView1.Rows.Add("", a, "");
+				}
+			}
+		}
+		public void mythread3()
+		{
+			lock (loker)
+			{
+				int a = 2;
+				double b;
+				b = Math.Pow(a, 10);
+				dataGridView1.Rows.Add("", "", b);
+			}
+		}
+		
+		private void button1_Click(object sender, EventArgs e)
+		{
+			dataGridView1.Rows.Clear();
+			System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+			Thread thread1 = new Thread(mythread1);
+			if (textBox1.Text != "")
+			{
+				int prior1 = Convert.ToInt32(textBox1.Text);
+				if (prior1 == 1) thread1.Priority = ThreadPriority.Highest;
+				if (prior1 == 2) thread1.Priority = ThreadPriority.AboveNormal;
+				if (prior1 == 3) thread1.Priority = ThreadPriority.Normal;
+				if (prior1 == 4) thread1.Priority = ThreadPriority.BelowNormal;
+				if (prior1 == 5) thread1.Priority = ThreadPriority.Lowest;
+			}
+			thread1.Start();
+			textBox1.Text = "";
+
+			Thread thread2 = new Thread(mythread2);
+			if (textBox2.Text != "")
+			{
+				int prior2 = Convert.ToInt32(textBox2.Text);
+				if (prior2 == 1) thread2.Priority = ThreadPriority.Highest;
+				if (prior2 == 2) thread2.Priority = ThreadPriority.AboveNormal;
+				if (prior2 == 3) thread2.Priority = ThreadPriority.Normal;
+				if (prior2 == 4) thread2.Priority = ThreadPriority.BelowNormal;
+				if (prior2 == 5) thread2.Priority = ThreadPriority.Lowest;
+			}
+			thread2.Start();
+			textBox2.Text = "";
+
+			Thread thread3 = new Thread(mythread3);
+			if (textBox3.Text != "")
+			{
+				int prior3 = Convert.ToInt32(textBox3.Text);
+				if (prior3 == 1) thread3.Priority = ThreadPriority.Highest;
+				if (prior3 == 2) thread3.Priority = ThreadPriority.AboveNormal;
+				if (prior3 == 3) thread3.Priority = ThreadPriority.Normal;
+				if (prior3 == 4) thread3.Priority = ThreadPriority.BelowNormal;
+				if (prior3 == 5) thread3.Priority = ThreadPriority.Lowest;
+			}
+			thread3.Start();
+			textBox3.Text = "";
+
+
+
+
+		}
+	}
+}
